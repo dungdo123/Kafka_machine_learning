824913d30536728bd356f2ccf4c82e5082912deb
do huu dong <dhdo@vision-in.co.kr>
2022-06-03 12:47:11 +0900

    code update

---
+++ sample_app.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import json
+import threading
+import uuid
+
+from pathlib import Path
+from kafka import KafkaProducer, KafkaConsumer
+from time import sleep
+
+PATH = Path('data/')
+KAFKA_HOST = 'localhost:9092'
+df_test = pd.read_csv(PATH/'adult.test')
+df_test['json'] = df_test.apply(lambda x: x.to_json(), axis=1)
+messages = df_test.json.tolist()
+
+def start_producing():
+    producer = KafkaProducer(bootstrap_servers=KAFKA_HOST)
+    for i in range(200):
+        message_id = str(uuid.uuid4())
+        message = {'request_id': message_id, 'data': json.loads(messages[i])}
+
+        producer.send('app_messages', json.dumps(message).encode('utf-8'))
+        producer.flush()
+        print("\033[1;31;40m -- PRODUCER: Sent message with id {}".format(message_id))
+        sleep(2)
+
+def start_consuming():
+    consumer = KafkaConsumer('app_messages', bootstrap_servers=KAFKA_HOST)
+
+    for msg in consumer:
+        message = json.loads(msg.value)
+        if 'prediction' in message:
+            request_id = message['request_id']
+            print("\033[1;32;40m ** CONSUMER: Received prediction {} for request id {}".format(message['prediction'],
+                                                                                               request_id))
+
+threads = []
+t = threading.Thread(target=start_producing())
+t2 = threading.Thread(target=start_consuming())
+threads.append(t)
+threads.append(t2)
+t.start()
+t2.start()
---
+++ trainer.py
@@ -0,0 +1,46 @@
+import pdb
+import json
+import pandas as pd
+import pickle
+import argparse
+from pathlib import Path
+from kafka import KafkaConsumer
+from utils.messages_utils import publish_training_completed
+from utils.preprocess_data import build_train
+
+KAFKA_HOST = 'localhost:9092'
+RETRAIN_TOPIC = 'retrain_topic'
+PATH = Path('data/')
+TRAIN_DATA = PATH/'train/train.csv'
+DATAPROCESSORS_PATH = PATH/'dataprocessors'
+MODELS_PATH = PATH/'models'
+MESSAGES_PATH = PATH/'messages'
+
+def train(model_id, messages, hyper):
+    print("Retraining started (model id: {})".format(model_id))
+    dtrain = build_train(TRAIN_DATA, DATAPROCESSORS_PATH, model_id, messages)
+    if hyper == "hyperopt":
+        from train.train_hyperopt_mlflow import LGBOptimizer
+    elif hyper == "hyperparameterhunter":
+        from train.train_hyperparameterhunter_mlflow import LGBOptimizer
+    LGBOpt = LGBOptimizer(dtrain, MODELS_PATH)
+    LGBOpt.optimizer(maxevals=2, model_id=model_id)
+    print("Retraining completed (model id: {}").format(model_id))
+
+def start(hyper):
+    consumer = KafkaConsumer(RETRAIN_TOPIC, bootstrap_servers=KAFKA_HOST)
+    for msg in consumer:
+        message = json.loads(msg.value)
+        if 'retrain' in message and message['retrain']:
+            model_id = message['model_id']
+            batch_id = message['batch_id']
+            message_fname = 'messages_{}_.txt'.format(batch_id)
+            messages = MESSAGES_PATH/message_fname
+
+            train(model_id, messages, hyper)
+            publish_training_completed(model_id)
+if __name__ = '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hyper", type=str, default="hyperopt")
+    args = parser.parse_args()
+    start(args.hyper)
